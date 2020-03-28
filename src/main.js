fafead8ef6c1e247eacff53cba8254973939f149
PVPoyer <pieter.vanpoyer@portofantwerp.com>
2020-03-28 15:48:55 +0100

    Eerste versie van de phaser demo 3

--- src/main.js
+++ src/main.js
@@ -19,11 +19,6 @@ class MijnPhaserScene extends Phaser.Scene {
 
     create() {
         this.add.image(400, 300, 'sky');
-        this.add.image(400, 300, 'star');
-        this.add.image(150, 150, 'star');
-        this.add.image(150, 450, 'star');
-        this.add.image(600, 450, 'star');
-        this.add.image(600, 150, 'star');
 
         this.platforms = this.physics.add.staticGroup();
 
@@ -35,7 +30,7 @@ class MijnPhaserScene extends Phaser.Scene {
 
         this.player = this.physics.add.sprite(100, 450, 'dude');
 
-        this.player.setBounce(0.3);
+        this.player.setBounce(0.1);
         this.player.setCollideWorldBounds(true);
 
         this.anims.create({
@@ -58,11 +53,36 @@ class MijnPhaserScene extends Phaser.Scene {
             repeat: -1
         });
 
-        this.player.body.setGravityY(300)
+        this.player.body.setGravityY(300);
 
         this.physics.add.collider(this.player, this.platforms);
 
         this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 },
+        });
+
+        this.stars.children.iterate( (child) => {
+            child.setBounceY(Phaser.Math.FloatBetween(0.1, 0.2));
+        });
+
+        this.physics.add.collider(this.stars, this.platforms);
+
+        this.physics.add.overlap(this.player, this.stars, (player, star) => {
+            this.collectStar(player, star)
+        }, null, this);
+
+        this.score = 0;
+        this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
+        this.bombs = this.physics.add.group();
+
+        this.physics.add.collider(this.bombs, this.platforms);
+
+        this.physics.add.collider(this.player, this.bombs, (player, bomb) => this.hitBomb(player, bomb), null, this);
     }
 
     update() {
@@ -83,6 +103,33 @@ class MijnPhaserScene extends Phaser.Scene {
         if (this.cursors.up.isDown && this.player.body.touching.down) {
             this.player.setVelocityY(-500);
         }
+    }
+
+    collectStar(player, star) {
+        star.disableBody(true, true);
+
+        this.score += 10;
+        this.scoreText.setText('Score: ' + this.score);
+        if (this.stars.countActive(true) === 0) {
+            this.stars.children.iterate(function (child) {
+                child.enableBody(true, child.x, 0, true, true);
+            });
+
+            const x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+            const bomb = this.bombs.create(x, 16, 'bomb');
+            bomb.setBounce(1);
+            bomb.setCollideWorldBounds(true);
+            bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        }
+    }
+
+    hitBomb(player, bomb) {
+        this.physics.pause();
+        this.player.setTint(0xff0000);
+
+        this.player.anims.play('turn');
+        this.gameOver = true;
     }
 }
 
